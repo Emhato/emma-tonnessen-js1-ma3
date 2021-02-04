1a7e11664ec1f185ef359cfe0ac063e2eec004e7
Emhato <71319235+Emhato@users.noreply.github.com>
2021-02-04 22:28:51 +0100

    ahhhh

---
+++ js/question-1.js
@@ -0,0 +1,11 @@
+// Question 1
+// Convert the function below to an arrow function:
+
+const division = (a, b) => a % b;
+
+// The functin can for example be called like this:
+console.log(division(3, 2));
+
+// Or like this:
+// const result = division(3, 2);
+// console.log(result);
